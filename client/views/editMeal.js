0008514a50758a8349a8631d28f0c90f24fb1293
Godsmith <filip.lange@gmail.com>
2015-10-16 23:50:30 +0200

    Now displays the name of a meal when clicking on it.

--- client/views/editMeal.js
+++ client/views/editMeal.js
@@ -1,5 +1,8 @@
 Template.editMeal.helpers({
   display: function() {
     return Session.get('displayEditMeal') ? '' : 'display: none;';
+  },
+  meal: function() {
+    return Session.get('meal');
   }
 });
