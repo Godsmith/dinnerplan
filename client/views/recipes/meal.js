aa74f6ea2edc4ae5136f1348ee41580aaa15a328
Godsmith <filip.lange@gmail.com>
2016-01-08 00:07:45 +0100

    Only show name, time and rating in recipe list

--- client/views/recipes/meal.js
+++ client/views/recipes/meal.js
@@ -1,7 +1,7 @@
 Template.meal.helpers({
   parameters: function() {
     let retVal = [];
-    for (let mealProperty of MEAL_PROPERTIES) {
+    for (let mealProperty of MEAL_PROPERTIES_TO_SHOW_IN_LIST) {
       retVal.push(
         {
           value: this[mealProperty.databaseKeyName],
--- client/views/recipes/recipesTable.js
+++ client/views/recipes/recipesTable.js
@@ -1,5 +1,5 @@
 Template.recipesTable.helpers({
   mealProperties: function() {
-    return MEAL_PROPERTIES;
+    return MEAL_PROPERTIES_TO_SHOW_IN_LIST;
   }
 });
